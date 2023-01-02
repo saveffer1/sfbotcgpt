6d7aac9b29219a4deb7f3d90e98e05993234a565
wiraphat prasomphong <69828979+saveffer1@users.noreply.github.com>
2023-01-02 18:16:21 +0700

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -304,3 +304,10 @@ def run_discord_bot():
     
     TOKEN = config['discord_bot_token']
     client.run(TOKEN)
+    
+    print("Watchdog start:")
+    while True: # run forever
+        print("Watchdog: process started")  # program started message
+        exec(open("./watchdog.py").read())   # run the program
+        print("Watchdog: process finished") # program stopped message
+    print("Watchdog end:") # the watchdog is now finishing
