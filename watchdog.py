222fec1affbf073b86b93ddfa41eea792cc0241c
wiraphat prasomphong <69828979+saveffer1@users.noreply.github.com>
2023-01-02 18:02:51 +0700

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -1,6 +1,6 @@
-print("Watchdog start:")
-while True: # run forever
-    print("Watchdog: process started")  # program started message
-    exec(open("./program.py").read())   # run the program
-    print("Watchdog: process finished") # program stopped message
-print("Watchdog end:") # the watchdog is now finishing
+import time
+
+for i in range (0, 5):
+    print(i)
+    time.sleep(1)
+print("I done")
