3d8092756b0400ec987519e84418a64fa4fc30fe
wiraphat prasomphong <69828979+saveffer1@users.noreply.github.com>
2023-01-02 18:14:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,4 @@ def check_verion() -> None:
 
 if __name__ == '__main__': 
     check_verion()
-    print("Watchdog start:")
-    while True: # run forever
-        print("Watchdog: process started")  # program started message
-        exec(open("./watchdog.py").read())   # run the program
-        print("Watchdog: process finished") # program stopped message
-    print("Watchdog end:") # the watchdog is now finishing
     bot.run_discord_bot()
